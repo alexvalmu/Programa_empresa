cc78c1fe05079f9b80f480437efd716cfcbb34ea
carlos2004hb <118807142+carlos2004hb@users.noreply.github.com>
2022-11-29 18:16:48 +0100

    Add files via upload

---
+++ propuestaproyecto.c
@@ -0,0 +1,30 @@
+//Grupos de 3 estudiantes. Se desarrolla en C estándar bajo Linux. Debe funcionar en los ordenadores de los laboratorios. Diseñado empleando módulos y paso de parámetros
+//Prohibido el uso de variables globales. Obligatorio el uso de arrays y registros. Solo se pueden emplear los tipos de datos estudiados en la asignatura
+//Posibilidad de realización en modo texto o modo gráfico empleando la librería gfx. La utilización de otras tecnologías requiere el visto bueno del profesorado
+//La asistencia a prácticas es obligatoria durante estas sesiones. Los grupos deben crearse durante las sesiones de la semana del 21 de noviembre
+
+//- Una empresa quiere implementar un sistema de registro de empleados, con sus nombres, puestos y sus horas de entrada y salida. 
+//Realiza un programa que permita realizar varias opciones, como ingresar un nuevo empleado (sólo alguno con un puesto determinado), 
+//fichar una entrada o salida (uno mismo), ver el resumen de horarios por empleados. 
+//Todo ello se podría gestionar mediante un código de empleado, dependiendo de la acción a realizar.
+//array de registros y uno de ellos tiene que ser ordenado, selector de registros y sintetizar 
+
+//Cuando incias el programa, necesitarás introducir una contraseña - Tomas
+//Director: horarios de entrada y salida, contratar gente entre candidatos posibles (sin usuario externo), despedir gente, contador de quejas - Director
+//Trabajador: tu hora de entrada y de salida, sueldo actual, pedir aumento, ver horario, horas trabajadas, introducir quejas - Alex
+//Usuario externo: economía de la empresa, descripción de empresa, ver datos de empresa (+puestos de trabajos disponibles) - Tomas
+
+//Es decir, el director y trabajador hacen funciones y el usuario externo es el que va a comprobar todos los datos
+
+//Director: Eduard Gómez Aguilar 78900754F
+ 
+//Puestos de trabajo: Ingenieros, Ventas y Logística
+
+//Ingenieros: Tomas Woodward Marín 89700456K , María Magdalena Esteve 45678321C
+
+//Ventas: Carlos Hurtado Baeza 9907653Z, Carolina Sánchez Abad 54670987I
+
+//Logisitica: Alex Valdelvira Muñoz 44678217X , Rosana Pérez Vazquez 90678543P
+
+//Usuario externo: Nombre a introducir
+
